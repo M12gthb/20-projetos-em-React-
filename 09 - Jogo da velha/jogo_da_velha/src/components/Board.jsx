1a43c0bd80de0c4fa3fce9a67db74ec48ab6f790
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-05-16 10:43:43 -0300

    feat: game complete

--- 09 - Jogo da velha/jogo_da_velha/src/components/Board.jsx
+++ 09 - Jogo da velha/jogo_da_velha/src/components/Board.jsx
@@ -3,35 +3,106 @@ import Square from "./Square";
 
 function Board() {
   const [isNext, setIsNext] = useState(true);
-  const winner = false;
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [aiIsThinking, setAiIsThinking] = useState(false);
+  const winner = calculateWinner(squares);
+
+  useEffect(() => {
+    if (!isNext && !winner) {
+      setAiIsThinking(true);
+      setTimeout(() => {
+        aiMove(squares, setSquares, setIsNext);
+        setAiIsThinking(false);
+      }, 1000);
+    }
+  }, [isNext, squares, winner]);
+
+  const handleClick = (i) => {
+    if (squares[i] || winner || aiIsThinking) return;
+
+    const newSquares = squares.slice();
+
+    newSquares[i] = isNext ? "X" : "O";
+
+    setSquares(newSquares);
+
+    setIsNext(!isNext);
+  };
+
+  const resetGame = () => {
+    setSquares(Array(9).fill(null));
+    setIsNext(true);
+  };
+
   return (
     <div className="board">
       <div className="status">
         Status:{" "}
         {winner ? (
-          <p>O vencedor é:{winner}!</p>
+          <p className="winner">O vencedor é: {winner}!</p>
         ) : (
           `Próximo a jogar: ${isNext ? "X" : "O"}`
         )}
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
-      <button className="reset-button">Reiniciar o jogo</button>
+      <button className="reset-button" onClick={() => resetGame()}>
+        Reiniciar o jogo
+      </button>
     </div>
   );
 }
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
+const aiMove = (squares, setSquares, setXIsNext) => {
+  let move = null;
+  for (let i = 0; i < squares.length; i++) {
+    if (!squares[i]) {
+      move = Math.floor(Math.random() * (8 - 0 + 1)) + 0;
+      break;
+    }
+  }
+
+  if (move !== null) {
+    const newSquares = squares.slice();
+    newSquares[move] = "O";
+    setSquares(newSquares);
+    setXIsNext(true);
+  }
+};
 
 export default Board;
