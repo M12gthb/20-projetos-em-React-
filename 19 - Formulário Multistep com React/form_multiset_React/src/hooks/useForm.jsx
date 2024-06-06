1451eff569c0a97062e5a8279eedeb4c69800603
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-06-06 15:32:05 -0300

    feat: add components

---
+++ 19 - Formulário Multistep com React/form_multiset_React/src/hooks/useForm.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function useForm(steps) {
+  const [currentStep, setCurrentStep] = useState(0);
+
+  function changeStep(i, e) {
+    if (e) e.preventDefault();
+
+    if (i < 0 || i >= steps.length) return;
+
+    setCurrentStep(i);
+  }
+
+  return {
+    currentStep,
+    isLastStep: currentStep + 1 === steps.length ? true : false,
+    isFristStep: currentStep === 0 ? true : false,
+    currentComponent: steps[currentStep],
+    changeStep,
+  };
+}
+
+export default useForm;
